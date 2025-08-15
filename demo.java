3f3e9bc48fbb321489b4543be85d031d5dd791c7
Yuheswari <yuheswari2525@gmail.com>
2025-08-15 20:47:46 +0530

    Resolved merge conflicts

---
+++ demo.java
@@ -0,0 +1,11 @@
+public class demo{
+    public static void main(String[]args){
+        int row =3;
+        for(int i=0;i<row;i++){
+            for(int j=0;j<row;j++){
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+}
---
+++ reverse.java
@@ -0,0 +1,11 @@
+public class reverse{
+    public static void main(String[]args){
+        int row =5;
+        for(int i=row;i>=0;i--){
+            for(int j=0;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+}
