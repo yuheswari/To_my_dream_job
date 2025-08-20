242fd76637bb61f5c92a0abaf8d81b0cdbf64d4d
Yuheswari <yuheswari2525@gmail.com>
2025-08-20 20:40:59 +0530

    factorial

---
+++ loops/fact.java
@@ -0,0 +1,10 @@
+public class fact{
+    public static void main(String[]args){
+        int fact=0;
+        int n=5;
+        for(int i=0;i<n;i++){
+            fact=fact*i;
+        }
+        System.out.println(fact);
+    }
+}
--- practice/sumof.java
+++ practice/sumof.java
@@ -1,6 +1,15 @@
 import java.util.*;
 public class sumof{
     public static void main(String[]args){
-        
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[]=new int[n];
+        int sum =0;
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+            
+        }
+
+
     }
 }
